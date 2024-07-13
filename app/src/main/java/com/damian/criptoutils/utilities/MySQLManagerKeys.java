c8f18d5f4d22c895d99c660ded935ac57235ad42
Damian <bdpmxd2@gmail.com>
2024-07-13 20:02:11 +0200

    Fixed hide database connection information in Git, but provided sample data

---
+++ app/src/main/java/com/damian/criptoutils/utilities/MySQLManagerKeys.java
@@ -0,0 +1,11 @@
+package com.damian.criptoutils.utilities;
+
+public class MySQLManagerKeys {
+
+    public static String db = "your_database";
+    public static String ip = "your_ip";
+    public static String port = "your_port";
+    public static String username = "your_username";
+    public static String password = "your_password";
+
+}
