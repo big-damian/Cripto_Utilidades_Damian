17bedce017fe65bb3f78fd0db5f89cb2583cf18a
Damian <bdpmxd2@gmail.com>
2024-07-13 19:50:27 +0200

    Fixed hide database connection information in Git

--- app/src/main/java/com/damian/criptoutils/utilities/MySQLManager.java
+++ app/src/main/java/com/damian/criptoutils/utilities/MySQLManager.java
@@ -9,23 +9,11 @@ import java.util.Objects;
 // Sacado de este video que es GOD: https://www.youtube.com/watch?v=mbHvLSwhdLA
 public class MySQLManager {
 
-//    protected static String db = "criptoutilsdamian";
-//    protected static String ip = "192.168.1.145";
-//    protected static String port = "3306";
-//    protected static String username = "criptoutilsdamian-app";
-//    protected static String password = "androideSQL";
-
-//    protected static String db = "sql7709822";
-//    protected static String ip = "sql7.freemysqlhosting.net";
-//    protected static String port = "3306";
-//    protected static String username = "sql7709822";
-//    protected static String password = "CYKg6hIxUZ";
-
-    protected static String db = "bxesejh9qzvvn66mxisn";
-    protected static String ip = "bxesejh9qzvvn66mxisn-mysql.services.clever-cloud.com";
-    protected static String port = "3306";
-    protected static String username = "ujoqamr8zjksuld9";
-    protected static String password = "xgWOrmU75XsNqCrZ1SDV";
+    protected static String db = MySQLManagerKeys.db;
+    protected static String ip = MySQLManagerKeys.ip;
+    protected static String port = MySQLManagerKeys.port;
+    protected static String username = MySQLManagerKeys.username;
+    protected static String password = MySQLManagerKeys.password;
 
     public Connection ConexionMySQL() {
         Connection cone = null;
