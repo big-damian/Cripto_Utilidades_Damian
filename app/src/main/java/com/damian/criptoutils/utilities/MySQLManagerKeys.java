f8a248f3f30737f5debd1a9055271c2f893a56aa
Damian <bdpmxd2@gmail.com>
2024-07-13 20:38:55 +0200

    Keys test 1

--- app/src/main/java/com/damian/criptoutils/utilities/MySQLManagerKeys.java
+++ app/src/main/java/com/damian/criptoutils/utilities/MySQLManagerKeys.java
@@ -7,5 +7,6 @@ public class MySQLManagerKeys {
     public static String port = "your_port";
     public static String username = "your_username";
     public static String password = "your_password";
+    // Test1
 
 }
