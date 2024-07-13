ea78cda96bddce8354b307df75cfd83285b9f48c
Damian <bdpmxd2@gmail.com>
2024-07-13 20:55:47 +0200

    Added comments to explain how to provide database connection information and make the app work.

--- app/src/main/java/com/damian/criptoutils/utilities/MySQLManager.java
+++ app/src/main/java/com/damian/criptoutils/utilities/MySQLManager.java
@@ -9,6 +9,21 @@ import java.util.Objects;
 // Sacado de este video que es GOD: https://www.youtube.com/watch?v=mbHvLSwhdLA
 public class MySQLManager {
 
+//    HERE IM USING ANOTHER CLASS CALLED MySQLManagerKeys.java in com.damian.criptoutils.utilities TO STORE THE KEYS AND ASSIGN THEM INTHE LINES BELOW
+//    THE CLASS LOOKS LIKE THIS, CREATE THIS SAME CLASS OR ASSIGN THE KEYS DIRECTLY TO THE VARIABLES FOR THE APP TO WORK:
+//
+//    package com.damian.criptoutils.utilities;
+//
+//    public class MySQLManagerKeys {
+//
+//        public static String db = "my_database";
+//        public static String ip = "my_ip";
+//        public static String port = "my_port";
+//        public static String username = "my_username";
+//        public static String password = "my_password";
+//
+//    }
+
     protected static String db = MySQLManagerKeys.db;
     protected static String ip = MySQLManagerKeys.ip;
     protected static String port = MySQLManagerKeys.port;
